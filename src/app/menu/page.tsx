d38e3985241164bb25c90da2f074f2c25f951924
John Manalac <jmanalac@middlebury.edu>
2023-12-19 12:17:06 -0800

    Initial commit

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Menu',
+    description: 'Menu',
+};
+
+export default function MenuPage() {
+  return (
+    <div>page</div>
+  )
+}
